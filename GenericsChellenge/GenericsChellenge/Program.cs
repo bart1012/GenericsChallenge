7af7f2c4c421ae2779193cb160a9f51b22f38cde
bart <bielski.bart1012@gmail.com>
2024-11-15 10:10:53 +0000

    Initial commit

---
+++ GenericsChellenge/GenericsChellenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsChellenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
